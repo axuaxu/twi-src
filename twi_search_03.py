42c5bbd1ed19e6cc70a7ed1c3f7ad9a2bec94a18
axuaxu <axu0110@gmail.com>
2017-10-02 13:10:57 -0400

    3

--- twi_search_03.py
+++ twi_search_03.py
@@ -3,6 +3,8 @@ from credentials import *
 import sys
 #import jsonpickle
 import os
+import codecs
+import datetime
 
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 #auth.set_access_token(access_token, access_token_secret)
@@ -14,20 +16,20 @@ if (not api):
     sys.exit(-1)
 
 
-def TwiSearch(que,inName):
+def TwiSearch(que,fName,sinceId,max_id):
     searchQuery = que
       # this is what we're searching for
     maxTweets = 10000000 # Some arbitrary large number
-    fName = inName
+     
     tweetsPerQry = 100  # this is the max the API permits
     # We'll store the tweets in a text file.
     # If results from a specific ID onwards are reqd, set since_id to that ID.
     # else default to no lower limit, go as far back as API allows
-    sinceId = None
+    
 
     # If results only below a specific ID are, set max_id to that ID.
     # else default to no upper limit, start from the most recent tweet matching the search query.
-    max_id = -1L
+    
 
     tweetCount = 0
     print("Downloading max {0} tweets".format(maxTweets))
@@ -66,6 +68,30 @@ def TwiSearch(que,inName):
     print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
 
 
-que = '#HenriMatisse'
-inName = 'tweets.txt'
-TwiSearch(que,inName)
+#que = '#georgiaokeeffe'
+#outName = 'tweets-01.txt'
+sinceId = None
+max_id = -1L
+
+#TwiSearch(que,outName,sinceId,max_id)
+now =  datetime.datetime.now()
+timestr = str(now).replace(' ','-').replace(':','-')
+#print str(now)+'\n'+timestr+'\n'+timestr[:19]
+
+twi = "twi_query_list.txt"
+#fout = codecs.open(out,"w",encoding="utf-8")
+ftwi = codecs.open(twi,'r',encoding="utf-8")
+count = 100
+cdir = '.\\status-query'
+srcDown = '.\\status-query-down'
+maxid = 9999999999999999999
+for que in ftwi: 
+     #maxid = getMaxID(que,cdir,srcDown,sinceId,max_id)
+     #print "twi_id:"+twi_id+" maxid:"+str(maxid)
+     out = cdir+'\\que-'+que+'-'+timestr[:16]+'.txt'
+     out = out.replace('\r\n','')
+     tstatus = TwiSearch(que,out,sinceId,max_id)
+     
+     #fout = codecs.open(out,"w",encoding="utf-8")
+     #fout.write(tstatus)
+     print "input: "+ que+"\noutput: "+out

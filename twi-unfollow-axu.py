d95a411fec099fd828348eee902d9067fc2aa0fd
axuaxu <axu0110@gmail.com>
2017-10-10 22:12:52 -0400

    3

---
+++ twi-unfollow-axu.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+import tweepy
+from credentials import * 
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+SCREEN_NAME = 'lisaeme75659217'
+    
+followers = api.followers_ids(SCREEN_NAME)
+friends = api.friends_ids(SCREEN_NAME)
+
+i = 0
+count = 20 
+for f in friends:
+  if i< count:
+    i = i+1
+    print i
+    if f not in followers:
+        print "Unfollow {0}".format(api.get_user(f).screen_name)
+        #if raw_input("Y/N?") == 'y' or 'Y':
+        api.destroy_friendship(f)
